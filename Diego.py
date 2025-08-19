aa23cf41d970954d2c1b77be28051e99819911cf
dmechavarriausistemas-hash <dmechavarria_usistemas@intep.edu.co>
2025-08-19 15:15:40 -0500

    Add files via upload

---
+++ Diego.py
@@ -0,0 +1,15 @@
+print("hola mundo")
+nombre = "Diego "
+apellido = "Echavarria"
+print(nombre + apellido)
+numero1 = 50
+numero2 = 10
+
+multiplicar = numero1 * numero2
+dividir = numero1 / numero2
+sumar = numero1 + numero2
+restar = numero1 - numero2
+print(multiplicar)
+print (dividir)
+print(sumar)
+print(restar)
